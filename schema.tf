9b2aad9f1ef9f96a1c757eb22613f12ac238dc1d
hannamchun <149615705+hannamchun@users.noreply.github.com>
2024-04-30 10:36:37 -0500

    Update schema.tf

--- schema.tf
+++ schema.tf
@@ -1,5 +1,5 @@
 resource "databricks_schema" "myschema" {
-    catalog_name = "main"
+    catalog_name = "hanna_chun"
     name         = "myschema_tfc"
     comment      = "This schema is managed by Terraform Cloud"
 }
